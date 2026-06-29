d6a68e8e5fe7b8f08e756cff1e052a776d408615
Surpreet Kaur <surpreet@usc.edu>
2026-06-29 20:03:21 +0000

    lab 8 done

--- lab8/bst.cpp
+++ lab8/bst.cpp
@@ -12,25 +12,70 @@
 using namespace std;
 
 int rangeSum(Node *root, int L, int R) {
-	
-	//Base case - called on a null node -> return 0!	
-	
-	//Otherwise get the value of the left and right trees under you (recursive call)
-	//Sum em
-	//Should add node->key to the sum?
-	//Return the sum!
 
-	return 0;
+    //Base case - called on a null node -> return 0!
+    if (root == nullptr)
+    {
+        return 0;
+    }
+
+    //Otherwise get the value of the left and right trees under you (recursive call)
+    //Sum em
+    //Should add node->key to the sum?
+    //Return the sum!
+
+    if (root->key < L)
+    {
+        return rangeSum(root->right, L, R);
+    }
+
+    if (root->key > R)
+    {
+        return rangeSum(root->left, L, R);
+    }
+
+    return root->key +
+           rangeSum(root->left, L, R) +
+           rangeSum(root->right, L, R);
 }
-    
-bool isBalanced(Node *root) {
-	//Get the heights of the left and right subtrees - helper func?
-	//Determine if this node is balanced! If not ret false!
 
-	//Check if there are subtrees under us
-	//Are they balanced?
+int heightCheck(Node* root)
+{
+    if (root == nullptr)
+    {
+        return 0;
+    }
 
-	//If all nodes are balanced return true!
+    int left = heightCheck(root->left);
+    if (left == -1)
+    {
+        return -1;
+    }
+
+    int right = heightCheck(root->right);
+    if (right == -1)
+    {
+        return -1;
+    }
+
+    if (abs(left - right) > 1)
+    {
+        return -1;
+    }
+
+    return 1 + max(left, right);
+}
+
+bool isBalanced(Node *root)
+{
+    //Get the heights of the left and right subtrees - helper func?
+    //Determine if this node is balanced! If not ret false!
+
+    //Check if there are subtrees under us
+    //Are they balanced?
+
+    //If all nodes are balanced return true!
+    return heightCheck(root) != -1;
 }
 
 /*
@@ -54,8 +99,43 @@ return its level order traversal as:
 ]
 
 */
-vector<vector<int>> levelOrder(Node* root) {
-	vector<vector<int>> results;
+vector<vector<int>> levelOrder(Node* root)
+{
+    vector<vector<int>> results;
+
+    if (root == nullptr)
+    {
+        return results;
+    }
+
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        int size = q.size();
+        vector<int> level;
+
+        for (int i = 0; i < size; i++)
+        {
+            Node* curr = q.front();
+            q.pop();
+
+            level.push_back(curr->key);
+
+            if (curr->left)
+            {
+                q.push(curr->left);
+            }
+
+            if (curr->right)
+            {
+                q.push(curr->right);
+            }
+        }
+
+        results.push_back(level);
+    }
+
     return results;
 }
-
